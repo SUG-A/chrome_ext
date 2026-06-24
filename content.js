8d27a10b1b26edd953f43c4edf46cc3167d0245f
SUG-A <athu01.0210@gmail.com>
2026-06-24 20:53:54 +0900

    一旦会社のやつとりこんだ

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
         // 1. 画面内のすべての「span.time.dep」要素をまとめて取得する
         // これにより、[<span class="time dep">06:30</span>, <span class="time dep">06:45</span>, ...] のようなリストが取れます
-        const elements = document.querySelectorAll(".schedule-text");
+        const elements = document.querySelectorAll(".date-of-month.today .schedule span");
         const scheduleList = []; // オブジェクトの配列に変える
 
         // 2. 取得した要素から、時間と予定名を分割して配列のオブジェクトにする
@@ -15,8 +15,14 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
                 // 2個目以降の文字を結合して予定名にする（空白引越し対策）
                 const titlePart = parts.slice(1).join(" ") || "予定";
 
-                if (timePart.includes(":")) {
-                    scheduleList.push({ time: timePart, title: titlePart });
+                // [0]: 開始日
+                // [1]: 終了日
+                const timeRangeParts = timePart.split("~");
+
+                if (timeRangeParts[0].includes(":")) {
+                    scheduleList.push({ time: timeRangeParts[0], title: titlePart });
+                } else {
+                    alert(`時間の形式が不正です: ${timePart}`);
                 }
             }
         });
